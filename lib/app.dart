56d7e673ad4fe95b9b51c18f08345bfb5258a04f
Khang Dinh <khangdinh@Khangs-MBP.fritz.box>
2021-11-27 14:22:29 +0100

    refactor

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:aoeiv_leaderboard/pages/landing_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
