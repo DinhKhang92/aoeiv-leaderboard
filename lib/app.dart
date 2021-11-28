65072b13b770199723d4bf784314a174d17c9ca2
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2021-11-28 10:48:16 +0100

    add apptexts

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'package:aoeiv_leaderboard/config/styles/colors.dart';
 import 'package:aoeiv_leaderboard/pages/landing_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,10 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(centerTitle: false),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: primaryColor,
+        ),
       ),
-      home: const LandingPage(title: 'Flutter Demo Home Page'),
+      home: const LandingPage(),
+      supportedLocales: const [
+        Locale('en', ''),
+      ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
     );
   }
 }
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,47 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LandingPage extends StatefulWidget {
-  const LandingPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const LandingPage({Key? key}) : super(key: key);
 
   @override
   State<LandingPage> createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // appBar: AppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Stack(
+      children: [
+        _buildBackground(),
+        SafeArea(
+          bottom: false,
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.appTitle("1v1"),
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    InkWell(
+                      onTap: () => print("open-disclaimer"),
+                      child: const Icon(Icons.info_outline_rounded),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xffECECEC),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: AppLocalizations.of(context)!.searchbarHintText,
+                      hintStyle: const TextStyle(fontSize: 12, color: Color(0xff4E4E4E)),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+                      isDense: true,
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBackground() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff2C3B4D),
+            Color(0xff151925),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
