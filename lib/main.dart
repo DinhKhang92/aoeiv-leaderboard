6f46d028336641f3e0d549ea54091acbab554c18
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2021-12-01 23:04:47 +0100

    whiten status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:aoeiv_leaderboard/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.white,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
+  runApp(MyApp());
+}
