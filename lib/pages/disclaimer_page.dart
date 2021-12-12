fa94081c5a37ae0b1795f662eebbc977401858a2
Khang Dinh <khangdinh@Khangs-MBP.fritz.box>
2021-12-12 14:43:32 +0100

    refactor disclaimer page

--- lib/pages/disclaimer_page.dart
+++ lib/pages/disclaimer_page.dart
@@ -14,40 +14,51 @@ class DisclaimerPage extends StatelessWidget {
       body: Stack(
         children: [
           const Background(),
-          SafeArea(
-            bottom: false,
-            child: Container(
-              padding: const EdgeInsets.all(15),
-              child: Header(headerTitle: AppLocalizations.of(context)!.pageTitleDisclaimer),
-            ),
-          ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: Spacing.m.spacing),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                border: Border.all(color: kcPrimaryColor),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: AppLocalizations.of(context)!.disclaimerCaption,
-                      children: [
-                        const TextSpan(text: "\n\n"),
-                        TextSpan(text: AppLocalizations.of(context)!.disclaimerText),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          )
+          _buildHeader(context),
+          _buildDisclaimerContent(context),
         ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(Spacing.m.spacing),
+        child: Header(headerTitle: AppLocalizations.of(context)!.pageTitleDisclaimer),
+      ),
+    );
+  }
+
+  Widget _buildDisclaimerContent(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: Spacing.m.spacing),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          border: Border.all(color: kcPrimaryColor),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: _buildDisclaimerText(context),
+      ),
+    );
+  }
+
+  Widget _buildDisclaimerText(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(Spacing.m.spacing),
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            text: AppLocalizations.of(context)!.disclaimerCaption,
+            children: [
+              const TextSpan(text: "\n\n"),
+              TextSpan(text: AppLocalizations.of(context)!.disclaimerText),
+            ],
+          ),
+        ),
       ),
     );
   }
