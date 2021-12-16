c8f0f9848b60dea0d73d55a5e6326eba555c42d7
Khang Dinh <khangdinh@3171cb13-7a14-4314-b12b-7e48ab5ecca8.fritz.box>
2021-12-16 19:02:10 +0100

    fix test

--- test/unit/cubit/rating_history_data_cubit_test.dart
+++ test/unit/cubit/rating_history_data_cubit_test.dart
@@ -21,7 +21,7 @@ void main() {
     const int leaderboardId = 17;
     const int profileId = 1412;
 
-    final Rating exampleRating = Rating(rating: 1412, streak: 2, timestamp: 123, totalLosses: 2, totalWins: 8);
+    final Rating exampleRating = Rating(rating: 1412, streak: 2, timestamp: 123, totalLosses: 2, totalWins: 8, winRate: 70);
     final Player examplePlayer = Player(rank: 1412, name: "T0nb3rry", totalGames: 100, totalWins: 71, totalLosses: 29, mmr: 1412, profileId: 1412, winRate: 40);
 
     setUp(() {
