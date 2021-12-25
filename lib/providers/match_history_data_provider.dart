7dbd19f14b108829c855e55a6391f1e1bc4b12ff
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2021-12-25 04:35:16 +0100

    refactor

--- lib/providers/match_history_data_provider.dart
+++ lib/providers/match_history_data_provider.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:aoeiv_leaderboard/config/config.dart';
 import 'package:aoeiv_leaderboard/exceptions/fetch_data_exception.dart';
 import 'package:aoeiv_leaderboard/models/match.dart';
-import 'package:aoeiv_leaderboard/models/match_player.dart';
 import 'package:http/http.dart';
 
 class MatchHistoryDataProvider {
@@ -11,23 +10,14 @@ class MatchHistoryDataProvider {
   final Client _client = Client();
 
   Future<List<Match>> fetchMatchHistoryData(int profileId) async {
-    final String url = "${_config.matchHistoryBaseUrl}&profile_id=$profileId&count=${_config.maxCount}";
+    final String url = "${_config.matchHistoryBaseUrl}&profile_id=$profileId&count=${_config.matchHistoryMaxCount}";
 
-    final List<Match> matchList = [];
     final response = await _client.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final List jsonData = jsonDecode(response.body);
 
-      for (Map matchHistory in jsonData) {
-        final List<MatchPlayer> matchPlayers = (matchHistory['players'] as List).map((matchHistoryPlayer) => MatchPlayer.fromJSON(matchHistoryPlayer)).toList();
-
-        final Match match = Match.fromJSON(matchHistory);
-        match.setMatchPlayers = matchPlayers;
-
-        matchList.add(match);
-      }
-      return matchList;
+      return jsonData.map((matchHistory) => Match.fromJSON(matchHistory)).toList();
     }
 
     throw FetchDataException('Error ${response.statusCode}. Failed to fetch match history data with url: $url');
