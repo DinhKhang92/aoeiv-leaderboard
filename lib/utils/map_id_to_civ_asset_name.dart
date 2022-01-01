ac05147a90137581106023bb3925d1dafda72ff8
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2022-01-01 14:12:05 +0100

    add civ images

---
+++ lib/utils/map_id_to_civ_asset_name.dart
@@ -0,0 +1,22 @@
+String mapIdToCivAssetName(int id) {
+  switch (id) {
+    case 0:
+      return "abbasid";
+    case 1:
+      return "chinese";
+    case 2:
+      return "delhi";
+    case 3:
+      return "english";
+    case 4:
+      return "french";
+    case 5:
+      return "hre";
+    case 6:
+      return "mongols";
+    case 7:
+      return "rus";
+    default:
+      return "Unknown";
+  }
+}
