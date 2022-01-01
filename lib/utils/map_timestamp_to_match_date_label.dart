7adc53865e7b9738b2cb25dc3d165b8d6ca2bc4a
Khang Dinh <khangdinh@1781df68-4c5c-4b45-ac72-cf87afccc8c6.fritz.box>
2022-01-01 23:58:56 +0100

    add match date for minutes

--- lib/utils/map_timestamp_to_match_date_label.dart
+++ lib/utils/map_timestamp_to_match_date_label.dart
@@ -5,6 +5,11 @@ String mapTimestampToMatchDateLabel(BuildContext context, int timestamp) {
   final DateTime matchTimestamp = DateTime.fromMillisecondsSinceEpoch(timestamp);
   final DateTime now = DateTime.now();
 
+  final int minutes = now.difference(matchTimestamp).inMinutes;
+  if (minutes < 60) {
+    return AppLocalizations.of(context)!.playerDetailMatchHistoryTimeInMinutes(minutes);
+  }
+
   final int hours = now.difference(matchTimestamp).inHours;
   if (hours < 24) {
     return AppLocalizations.of(context)!.playerDetailMatchHistoryTimeInHours(hours);
@@ -15,11 +20,11 @@ String mapTimestampToMatchDateLabel(BuildContext context, int timestamp) {
     return AppLocalizations.of(context)!.playerDetailMatchHistoryTimeInDays(days);
   }
 
-  final int weeks = (days / 7).ceil();
+  final int weeks = (days / 7).floor();
   if (weeks < 5) {
     return AppLocalizations.of(context)!.playerDetailMatchHistoryTimeInWeeks(weeks);
   }
 
-  final int months = (weeks / 4).ceil();
+  final int months = (weeks / 4).floor();
   return AppLocalizations.of(context)!.playerDetailMatchHistoryTimeInMonths(months);
 }
