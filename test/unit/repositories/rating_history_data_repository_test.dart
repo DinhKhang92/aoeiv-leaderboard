38c41b1bcb9de9740e0dc107c19e4b7dfe6a5a8c
Khang Dinh <khangdinh@Khangs-MBP.fritz.box>
2022-01-03 21:39:31 +0100

    add tests

--- test/unit/repositories/rating_history_data_repository_test.dart
+++ test/unit/repositories/rating_history_data_repository_test.dart
@@ -29,6 +29,13 @@ void main() {
 
       expect(ratingHistoryData, [firstRatingDataPoint, secondRatingDataPoint]);
     });
+    test("should throw a no data exception if rating history data is empty", () async {
+      const String url = "https://aoeiv.net/api/player/ratinghistory?game=aoe4&leaderboard_id=17&profile_id=123123&count=1000";
+
+      when(_mockRatingHistoryDataProvider.fetchRatingHistoryData(any, url)).thenAnswer((_) async => []);
+
+      expect(() async => await _ratingHistoryDataRepository.fetchRatingHistoryData(17, 123123), throwsA(isA<NoDataException>()));
+    });
     test("should fetch leaderboard data of a single player using the profile id", () async {
       const String url = "https://aoeiv.net/api/leaderboard?game=aoe4&leaderboard_id=17&profile_id=123123";
       final Player expectedPlayer = Player.fromJson(exampleLeaderboardPlayerOne);
