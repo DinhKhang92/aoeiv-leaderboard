9745decaf944fc3cdd9e5fe29b49cda0f23587fc
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2022-01-08 13:52:04 +0100

    add tests

---
+++ test/unit/cubit/favorites_cubit_test.dart
@@ -0,0 +1,57 @@
+import 'package:aoeiv_leaderboard/cubit/favorites_cubit.dart';
+import 'package:aoeiv_leaderboard/exceptions/fetch_data_exception.dart';
+import 'package:aoeiv_leaderboard/models/favorite.dart';
+import 'package:aoeiv_leaderboard/repositories/favorites_data_repository.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'favorites_cubit_test.mocks.dart';
+
+@GenerateMocks([FavoritesDataRepository])
+void main() {
+  final MockFavoritesDataRepository _mockFavoritesDataRepository = MockFavoritesDataRepository();
+
+  late FavoritesCubit _favoritesCubit;
+
+  setUp(() {
+    _favoritesCubit = FavoritesCubit(favoritesDataRepository: _mockFavoritesDataRepository);
+  });
+  group("FavoritesCubit", () {
+    blocTest<FavoritesCubit, FavoritesState>(
+      "emits FavoritesLoadinag and FavoritesLoaded after init",
+      build: () {
+        when(_mockFavoritesDataRepository.loadFavorites()).thenAnswer((realInvocation) async => [
+              Favorite.fromJSON(const {"leaderboard_id": 17, "profile_id": 123123, "name": "T0nb3rry"})
+            ]);
+
+        return _favoritesCubit;
+      },
+      act: (cubit) => cubit.init(),
+      expect: () => [isA<FavoritesLoading>(), isA<FavoritesLoaded>()],
+    );
+    blocTest<FavoritesCubit, FavoritesState>(
+      "emits FavoritesLoadinag and FavoritesLoaded after updating favorites",
+      build: () {
+        when(_mockFavoritesDataRepository.updateFavorites(17, 123123, "T0nb3rry")).thenAnswer((realInvocation) async => [
+              Favorite.fromJSON(const {"leaderboard_id": 17, "profile_id": 123123, "name": "T0nb3rry"})
+            ]);
+
+        return _favoritesCubit;
+      },
+      act: (cubit) => cubit.updateFavorites(17, 123123, "T0nb3rry"),
+      expect: () => [isA<FavoritesLoading>(), isA<FavoritesLoaded>()],
+    );
+    blocTest<FavoritesCubit, FavoritesState>(
+      "emits FavoritesLoadinag and FavoritesLoaded after fetching a favorite",
+      build: () {
+        when(_mockFavoritesDataRepository.fetchLeaderboardDataByProfileId(17, 123123)).thenThrow(FetchDataException("failed"));
+
+        return _favoritesCubit;
+      },
+      act: (cubit) => cubit.fetchFavorite(17, 123123),
+      expect: () => [isA<FavoritesLoading>(), isA<FavoritesError>()],
+    );
+  });
+}
---
+++ test/unit/repositories/favorites_repository_test.dart
@@ -0,0 +1,41 @@
+import 'package:aoeiv_leaderboard/exceptions/no_data_exception.dart';
+import 'package:aoeiv_leaderboard/models/player.dart';
+import 'package:aoeiv_leaderboard/providers/leaderboard_data_provider.dart';
+import 'package:aoeiv_leaderboard/repositories/favorites_data_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../test_utils.dart';
+import 'favorites_repository_test.mocks.dart';
+
+@GenerateMocks([LeaderboardDataProvider])
+void main() {
+  final MockLeaderboardDataProvider _mockLeaderboardDataProvider = MockLeaderboardDataProvider();
+  final FavoritesDataRepository _favoritesDataRepository = FavoritesDataRepository(leaderboardDataProvider: _mockLeaderboardDataProvider);
+
+  group("FavoritesDataRepository", () {
+    test("should fetch leaderboard data of a single player using the profile id", () async {
+      const String url = "https://aoeiv.net/api/leaderboard?game=aoe4&leaderboard_id=17&profile_id=123123";
+      final Player expectedPlayer = Player.fromJson(exampleLeaderboardPlayerOne);
+      when(_mockLeaderboardDataProvider.fetchLeaderboardData(any, url)).thenAnswer(
+        (_) async => {
+          ...exampleLeaderboardData,
+          "leaderboard": [exampleLeaderboardPlayerOne]
+        },
+      );
+
+      final Player playerData = await _favoritesDataRepository.fetchLeaderboardDataByProfileId(17, 123123);
+
+      expect(playerData, expectedPlayer);
+    });
+    test("should throw no data exception if no player data found", () async {
+      const String url = "https://aoeiv.net/api/leaderboard?game=aoe4&leaderboard_id=17&profile_id=123123";
+      when(_mockLeaderboardDataProvider.fetchLeaderboardData(any, url)).thenAnswer(
+        (_) async => {...exampleLeaderboardData, "leaderboard": []},
+      );
+
+      expect(() async => await _favoritesDataRepository.fetchLeaderboardDataByProfileId(17, 123123), throwsA(isA<NoDataException>()));
+    });
+  });
+}
