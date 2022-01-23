f3db77a1c7fece0de3d05f88c5e0f6fe47923fd6
Khang Dinh <khangdinh@Khangs-MacBook-Pro.fritz.box>
2022-01-23 11:56:01 +0100

    add cron job for firestore

--- firebase_crons/functions/src/index.ts
+++ firebase_crons/functions/src/index.ts
@@ -1,9 +1,40 @@
 import * as functions from "firebase-functions";
+import axios from 'axios';
 
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
-import axios from 'axios';
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+
 import { firebaseConfig } from "./config";
+
+export const scheduledUpdateFirestore = functions.pubsub.schedule("every 5 minutes").onRun((_) => {
+    initializeApp(firebaseConfig);
+
+    const firestore = getFirestore();
+
+
+    const baseUrl = 'https://aoeiv.net/api'
+    const leaderboardIds = [17, 18, 19, 20];
+
+    leaderboardIds.forEach((leaderboardId) => {
+        const url = `${baseUrl}/leaderboard?game=aoe4&leaderboard_id=${leaderboardId}&start=1&count=100`
+
+        axios.get(url)
+            .then(async (res: any) => {
+                if (res.data !== undefined && res.data['leaderboard'].length > 0) {
+                    await setDoc(doc(firestore, "leaderboard", `${leaderboardId}`), res.data);
+                    functions.logger.log(`updated firestore with url: ${url}`)
+                } else {
+                    functions.logger.error(`updated firestore failed with url: ${url}`)
+                }
+            })
+            .catch((error: any) => {
+                functions.logger.error(`error: ${error}`)
+            })
+    });
+
+    functions.logger.log("scheduledUpdateFirestore will be run every 5 minutes")
+});
 
 export const scheduledUpdateDatabase = functions.pubsub.schedule("every 5 minutes").onRun((_) => {
     const app = initializeApp(firebaseConfig);
@@ -31,6 +62,6 @@ export const scheduledUpdateDatabase = functions.pubsub.schedule("every 5 minute
             })
     });
 
-    functions.logger.log("This will be run every 5 minutes")
+    functions.logger.log("scheduledUpdateDatabase will be run every 5 minutes")
 });
 
